088b45b99e609395e9cea94e08ad723915bfbf84
99-Malik <usamaiftikhar718.com>
2025-06-11 16:28:23 +0500

    new changes

--- src/components/header/ACHeader.jsx
+++ src/components/header/ACHeader.jsx
@@ -17,7 +17,7 @@ const Header = () => {
     { name: "Siemens", href: "/company/siemens" },
     { name: "Samsung", href: "/company/samsung" },
     { name: "LG", href: "/company/lg" },
-    { name: "AC", href: "/ac-repair" },
+    { name: "AC Repair", href: "/ac-repair" },
   ];
   return (
     <div className="absolute z-50 flex w-screen items-center justify-center ">
